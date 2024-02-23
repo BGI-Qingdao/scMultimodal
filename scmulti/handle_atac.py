15a309e22c10b64950d6503a7039ffecf4ffb7f3
YaoLi3 <liyaoo6o@gmail.com>
2024-02-23 16:48:18 +0800

    add rds2h5ad function

--- scmulti/handle_atac.py
+++ scmulti/handle_atac.py
@@ -25,7 +25,8 @@ class scATAC:
                  fragment_dir=None,
                  fragment_metadata_fn=None,
                  rscript_path='/dellfsqd3/MGI_QINGDAO/USER/lishuangshuang/software/miniconda3/envs/scRNA_v2.4/lib/R/bin/Rscript',
-                 project=None):
+                 project=None,
+                 gene_name_col=None):
         self.atac_fn = atac_fn
         self.scrna_fn = scrna_fn
         self.gff = gff
@@ -33,17 +34,24 @@ class scATAC:
         self.metadata_fn = fragment_metadata_fn
         self.rscript_path = rscript_path
         self.name = project
+        self.gene_name_col = gene_name_col
 
     def transfer_label(self):
         subprocess.call(f'{self.rscript_path} label_transfer.R '
-                        f'--rna {self.scrna_fn} --peaks {self.atac_fn} --fragments {self.fragment_dir} '
-                        f'--metadata_fn {self.metadata_fn} --gff {self.gff} --name {self.name}',
+                        f'--rna {self.scrna_fn} '
+                        f'--peaks {self.atac_fn} '
+                        f'--fragments {self.fragment_dir} '
+                        f'--metadata_fn {self.metadata_fn} '
+                        f'--gff {self.gff} '
+                        f'--name {self.name} '
+                        f'--gene_name_col {self.gene_name_col}',
                         shell=True)
 
     def get_atac_rds(self):
-        res = subprocess.call(f'{self.rscript_path} 01.get_atac_rds.R -d {self.fragment_dir} -m {self.metadata_fn}',
-                              shell=True)
-        print(res)
+        subprocess.call(f'{self.rscript_path} 01.get_atac_rds.R '
+                        f'-d {self.fragment_dir} '
+                        f'-m {self.metadata_fn}',
+                        shell=True)
 
-    def rds2h5ad(self):
-        pass
+    def rds2h5ad(self, fn):
+        subprocess.call(f'{self.rscript_path} rds2h5ad.R {fn}', shell=True)
