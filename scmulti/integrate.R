d815415c3c81c49ac124985c31bac85512addc5a
YaoLi3 <liyaoo6o@gmail.com>
2024-03-22 10:03:02 +0800

    clean up r scripts

--- scmulti/integrate.R
+++ scmulti/integrate.R
@@ -12,24 +12,20 @@ suppressMessages(library(data.table))
 parser <- argparse::ArgumentParser()
 # Add the input directory argument
 parser$add_argument("--list", help = "library id list text file")
-parser$add_argument("-o", "--organ", help = "orgam/sample name")
+parser$add_argument("--data_path", required = TRUE, help = "Path to the directory containing peak.bed, barcodes.tsv and matrix.mtx files")
+parser$add_argument("-n", "--name", help = "orgam/sample name")
 # Parse the command-line arguments
 args <- parser$parse_args()
 
 print('------------------------------------------------------------------------------------------------------------')
-print(args$organ)
-# job_list <- c('DP8480004852TR_L01_3',
-#               'DP8480004851TR_L01_3',
-#               'DP8480004852TR_L01_2',
-#               'DP8480004851TR_L01_2')
-# /dellfsqd2/ST_OCEAN/USER/liyao1/11.evo_fish/DATA/03.Oryzias_melastigma/sample_ids
+print(args$name)
 df <- read.table(file = args$list, header = F, stringsAsFactors = F)
 job_list <- as.vector(t(df$V1))
 print(job_list)
 
 #### 1.create muti datasets combined peaks####
 # read all peak sets 
-datapath <- file.path('/dellfsqd2/ST_OCEAN/USER/liyao1/11.evo_fish/exp/03.Oryzias_melastigma/01.merge_libraries/', args$organ)
+datapath <- args$data_path
 # Create an empty list to store the data
 data_list <- list()
 gr_list <- c()
@@ -48,22 +44,16 @@ for (job in job_list){
 }
 
 # Create a unified set of peaks to quantify in each dataset
-#会有warning说有的scaffold在其他样本没有很正常，reduce不仅会合并相邻的还会保留特异的，这些特异的在别的时期没有
-#https://web.mit.edu/~r/current/arch/i386_linux26/lib/R/library/IRanges/html/inter-range-methods.html
-combined.peaks <- reduce(x = c(gr_list[[1]], gr_list[[2]], gr_list[[3]], gr_list[[4]]))
-# combined.peaks <- reduce(x = gr_list)
-# combined.peaks <- do.call(reduce, gr_list)
-#peakwidths <- width(combined.peaks)
+code <- paste("c(", paste0("gr_list[[", 1:length(job_list), "]]", collapse = ","), ")")
+combined.peaks <- reduce(eval(parse(text = code)))
 print(combined.peaks)
 
 #annotation obj
 read_gff <- function(gff_fn, choose_type=NULL, custom_column = NULL){
     # read file
-    gff = rtracklayer::import.gff(gff_fn)
-
+    gff <- rtracklayer::import.gff(gff_fn)
     # ensure 'gene_name', 'gene_id', 'gene_biotype' are in the metadata columns
-    column_names = colnames(GenomicRanges::mcols(gff))
-
+    column_names <- colnames(GenomicRanges::mcols(gff))
     # only select gene and protein coding genes
     if (!is.null(choose_type)){
         if ('type' %in% column_names){
@@ -71,11 +61,10 @@ read_gff <- function(gff_fn, choose_type=NULL, custom_column = NULL){
         }
     }
     if ('gene_biotype' %in% column_names){
-        gff = gff[gff$gene_biotype=='protein_coding']
+        gff <- gff[gff$gene_biotype=='protein_coding']
     }else{
         gff$gene_biotype <- "protein_coding"
     }
-
     # Check if 'gene_name' is missing and update it based on other columns
     if (!'gene_name'  %in% column_names){
       if ("gene_id" %in% column_names) {
@@ -92,7 +81,6 @@ read_gff <- function(gff_fn, choose_type=NULL, custom_column = NULL){
         stop("Unable to assign gene_name. No suitable metadata column found.")
       }
     }
-
     # Check if 'gene_id' is missing and update it based on other columns
     if (!'gene_id' %in% column_names){
       if ("gene_name" %in% column_names) {
@@ -106,7 +94,6 @@ read_gff <- function(gff_fn, choose_type=NULL, custom_column = NULL){
 print('Reading GTF/GFF gene annotation...')
 gff_fn <- '/dellfsqd2/ST_OCEAN/USER/liyao1/11.evo_fish/DATA/03.Oryzias_melastigma/GCF_002922805.2_ASM292280v2_genomic.gtf'
 gff <- read_gff(gff_fn, choose_type='gene')
-print(gff)
 
 seurat_obj_list <- list()
 for (job in job_list){
@@ -116,14 +103,13 @@ for (job in job_list){
                     stringsAsFactors = FALSE,
                     header = TRUE,
                     row.names = 1)
-  print(head(rownames(md)))
+
   # create fragment objects
   print('create fragment objects...')
   frags <- CreateFragmentObject(
     path = file.path(datapath,job, paste0(job, ".fragments.tsv.gz")),
     cells = rownames(md)
   )
-  print(frags)
 
   # 3.Quantify peaks in each dataset
   print('Create Feature Matrix...')
@@ -155,19 +141,34 @@ for (job in job_list){
   seurat_obj_list <- c(seurat_obj_list, obj)
 }
 
-####merge obj####
+###merge obj####
+# seurat_obj_list <- list()
+# for (job in job_list){
+#   d <- readRDS(file.path(datapath, paste0(job, '_atac.rds')))
+#   print(dim(d))
+#   seurat_obj_list <- c(seurat_obj_list, d)
+# }
+# print(seurat_obj_list[13])
+# seurat_obj_list <- seurat_obj_list[-13]
+
+for (d in seurat_obj_list){
+  print(dim(d))
+}
 print('Merging Seurat Objects...')
 combined <- merge(
   x = seurat_obj_list[[1]],
   y = seurat_obj_list[-1]
 )
 
+# combined <- readRDS('/dellfsqd2/ST_OCEAN/USER/liyao1/11.evo_fish/exp/03.Oryzias_melastigma/01.merge_libraries/fin/combinded2.rds')
 print('Processing merged Seurat Object...')
 combined <- RunTFIDF(combined)
 combined <- FindTopFeatures(combined, min.cutoff = 10)
 combined <- RunSVD(combined)
 combined <- RunUMAP(combined, dims = 2:50, reduction = 'lsi')
-saveRDS(combined,file.path(datapath, "combined.rds"))
+saveRDS(combined,file.path(datapath, "combinded.rds"))
+# combined <- readRDS('/dellfsqd2/ST_OCEAN/USER/liyao1/11.evo_fish/exp/03.Oryzias_melastigma/01.merge_libraries/fin/combinded3.rds')
+
 
 ####integrate atac####
 # Create an empty list to store the VariableFeatures results
@@ -184,7 +185,7 @@ integration.anchors <- FindIntegrationAnchors(
   object.list = seurat_obj_list,
   anchor.features = peaks.use,
   reduction = "rlsi",
-  dims = 2:30
+  dims = 2:50
 )
 saveRDS(integration.anchors,file.path(datapath, "integration.anchors.rds"))
 
@@ -197,26 +198,31 @@ integrated <- IntegrateEmbeddings(
 )
 saveRDS(integrated,file.path(datapath, "integrated.rds"))
 
+print('Making UMAP plots...')
 integrated <- RunUMAP(integrated, reduction = "integrated_lsi", dims = 2:30)
 p1 <- DimPlot(combined, group.by = "sample") + ggtitle("Merged")
 p2 <- DimPlot(integrated, group.by = "sample") + ggtitle("Integrated")
 (p1|p2)
-
 ggsave(file.path(datapath, "integrate.pdf"),width = 16,height = 7)
 
-integrated <- FindNeighbors(object = integrated, reduction = 'integrated_lsi', dims = 2:30)
-integrated <- FindClusters(object = integrated, verbose = FALSE, algorithm = 3)
-
-
-gene.activities <- GeneActivity(integrated)
-
-integrated[['RNA']] <- CreateAssayObject(counts = gene.activities)
-integrated <- NormalizeData(
-  object = integrated,
-  assay = 'RNA',
-  normalization.method = 'LogNormalize',
-  scale.factor = median(integrated$nCount_RNA)
-)
-
-saveRDS(integrated,file.path(datapath, "integrated_atac.rds"))
+print('DONE.')
 print('------------------------------------------------------------------------------------------------------------')
+
+# integrated <- readRDS(file.path(datapath, "integrated.rds"))
+# print('Processing integrated data...')
+# integrated <- FindNeighbors(object = integrated, reduction = 'integrated_lsi', dims = 2:30)
+# integrated <- FindClusters(object = integrated, verbose = FALSE, algorithm = 3)
+#
+# print('Creating gene activity matrix...')
+# gene.activities <- GeneActivity(integrated)
+# print('Creating assay object...')
+# integrated[['RNA']] <- CreateAssayObject(counts = gene.activities)
+# integrated <- NormalizeData(
+#   object = integrated,
+#   assay = 'RNA',
+#   normalization.method = 'LogNormalize',
+#   scale.factor = median(integrated$nCount_RNA)
+# )
+#
+# saveRDS(integrated, file.path(datapath, "integrated_atac.rds"))
+# print('------------------------------------------------------------------------------------------------------------')
--- scmulti/label_transfer.R
+++ scmulti/label_transfer.R
@@ -19,35 +19,55 @@ parser$add_argument("-f", "--fragments", help = "Path to ATAC fragments.tsv.gz f
 parser$add_argument("-g", "--gff", help = "Path to GFF/GTF gene annotation file")
 parser$add_argument("--gene_name_col", help = "gene name columns in to GFF/GTF gene annotation file")
 parser$add_argument("--name", help = "Project name")
+parser$add_argument("--celltype", default='celltype', help = "cell type label in meta.data")
 parser$add_argument("-o", "--output", help = "output directory")
 # Parse the command-line arguments
 args <- parser$parse_args()
 
 # Load scATAC data
+print('Loading scATAC data...')
 data.atac <- readRDS(args$atac)
+print(data.atac)
 # Load scRNA data
+print('Loading scRNA data...')
 scrna_fn <- args$rna
 data.rna <- readRDS(scrna_fn)
+print(str(data.rna))
+print(colnames(data.rna@meta.data))
+print(head(data.rna@meta.data[[args$celltype]]))
 
 # Identifying anchors between scRNA-seq and scATAC-seq datasets
 # transfer label
 # quantify gene activity
+print('Calculating Gene Activity...')
 gene.activities <- GeneActivity(data.atac, features = VariableFeatures(data.rna))
+print('Gene Activity Done')
 
 # add gene activities as a new assay
 data.atac[["ACTIVITY"]] <- CreateAssayObject(counts = gene.activities)
 
 # normalize gene activities
 DefaultAssay(data.atac) <- "ACTIVITY"
+print('Normalizing and Scaling data...')
 data.atac <- NormalizeData(data.atac)
 data.atac <- ScaleData(data.atac, features = rownames(data.atac))
 
 # Identify anchors
-transfer.anchors <- FindTransferAnchors(reference = data.rna, query = data.atac, features = VariableFeatures(object = data.rna), reference.assay = "RNA", query.assay = "ACTIVITY", reduction = "cca")
+print('Finding Transfer Anchors...')
+transfer.anchors <- FindTransferAnchors(reference = data.rna,
+                                        query = data.atac,
+                                        features = VariableFeatures(object = data.rna),
+                                        reference.assay = "RNA",
+                                        query.assay = "ACTIVITY",
+                                        reduction = "cca")
 print('-----Finished finding transfer anchors-----')
 
 # Annotate scATAC-seq cells via label transfer
-celltype.predictions <- TransferData(anchorset = transfer.anchors, refdata = data.rna@meta.data$celltype, weight.reduction = data.atac[["integrated_lsi"]], dims = 2:30)
+print('Transfering data...')
+celltype.predictions <- TransferData(anchorset = transfer.anchors,
+                                     refdata = data.rna@meta.data[[args$celltype]],
+                                     weight.reduction = data.atac[["integrated_lsi"]],
+                                     dims = 2:30)
 print('-----Finished transferring data-----')
 data.atac <- AddMetaData(data.atac, metadata = celltype.predictions)
 
@@ -55,22 +75,20 @@ saveRDS(data.atac, file.path(args$output, paste0(args$name, '.merged.atac.rds'))
 print('-----Saved results to disk-----')
 
 
-plot_annotation <- function(merged.atac, data.rna){
-  png(filename=file.path(args$output, "atac_predicted_id.png"))
-  p1 <- DimPlot(merged.atac, group.by = "predicted.id", label = TRUE) + NoLegend() + ggtitle("Predicted annotation")
-  print(p1)
-  dev.off()
-  p2 <- DimPlot(data.rna, group.by = "celltype", label = TRUE) + NoLegend() + ggtitle("Ground-truth annotation")
-  p1 | p2
+plot_annotation <- function(merged.atac, data.rna, celltype_label){
+  p1 <- DimPlot(merged.atac, reduction = 'integrated_lsi', group.by = "predicted.id", label = TRUE) + NoLegend() + ggtitle("Predicted annotation")
+  p2 <- DimPlot(data.rna, group.by = celltype_label, label = TRUE) + NoLegend() + ggtitle("Ground-truth annotation")
+  p1 + p2
+  ggsave(filename = file.path(args$output, "atac_predicted_id.png"))
 }
 
-plot_umap <- function(data.rna, merged.atac){
-  p1 <- DimPlot(data.rna, group.by = "celltype", label = TRUE) + NoLegend() + ggtitle("RNA")
-  p2 <- DimPlot(merged.atac, group.by = "orig.ident", label = FALSE) + NoLegend() + ggtitle("ATAC")
+plot_umap <- function(data.rna, merged.atac, celltype_label){
+  p1 <- DimPlot(data.rna, group.by = celltype_label, label = TRUE) + NoLegend() + ggtitle("RNA")
+  p2 <- DimPlot(merged.atac, reduction = 'integrated_lsi', group.by = "orig.ident", label = FALSE) + NoLegend() + ggtitle("ATAC")
   p1 + p2
   plot <- (p1 + p2) & xlab("UMAP 1") & ylab("UMAP 2") & theme(axis.title = element_text(size = 18))
   ggsave(filename = file.path(args$output, "atacseq_b4_integration.png"))#, height = 7, width = 12, plot = plot, quality = 50)
 }
 
-plot_annotation(data.atac, data.rna)
-plot_umap(data.rna, data.atac)
+plot_annotation(data.atac, data.rna, args$celltype)
+plot_umap(data.rna, data.atac, args$celltype)
